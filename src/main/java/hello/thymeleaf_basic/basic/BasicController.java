82fd98a56379087b663e2b0f6737abc6f7ed1791
baesm971013 <bobbyb537@naver.com>
2025-01-04 15:42:09 +0900

    feat: literal example

--- src/main/java/hello/thymeleaf_basic/basic/BasicController.java
+++ src/main/java/hello/thymeleaf_basic/basic/BasicController.java
@@ -83,6 +83,13 @@ public class BasicController {
 
     }
 
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+
+    }
+
 
 
     @Data
